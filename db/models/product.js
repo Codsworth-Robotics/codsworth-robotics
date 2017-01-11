050e5f9edafca741fa873b449341aaf0cb773a36
Joey Darbyshire <Josephmdarbyshire@gmail.com>
2017-01-11 15:51:17 -0500

    changed how ratings are saved and recieved for products

--- db/models/product.js
+++ db/models/product.js
@@ -30,20 +30,31 @@ const Product = db.define('products', {
     defaultValue: 0,
     allowNull: false
   },
-  averageRating: {
-    type: Sequelize.DOUBLE,
-    defaultValue: 0
-  },
-  totalRatings: {
-    type: Sequelize.INTEGER,
-    defaultValue: 0
+  // this is the total number of each star of ratings [1star, 2stars, 3stars, ...]
+  ratingsTotal: {
+    type: Sequelize.ARRAY(Sequelize.INTEGER),
+    defaultValue: [0, 0, 0, 0, 0]
   },
   images: {
     type: Sequelize.ARRAY(Sequelize.STRING),
     defaultValue: ['http://fillmurray.com/140/200']
   }
 }, {
-  indexes: [{fields: ['name'], unique: true}]
+  indexes: [{fields: ['name'], unique: true}],
+  getterMethods: {
+    averageRating: function () {
+      return this.ratingsTotal.reduce((val, idx) => {
+        return val * (idx + 1);
+      }, 0);
+    }
+  },
+  setterMethods: {
+    incrementRating: function (num) {
+      const oldTotal = this.getDataValue('ratingsTotal');
+      oldTotal[num - 1]++;
+      this.setDataValue('ratingsTotal', oldTotal);
+    }
+  }
 });
 
 
