d70f7c27ef731d4d1e5f7e9ff11a0cab52f39945
Beth Qiang <bethany.qiang@gmail.com>
2017-01-13 23:44:20 -0600

    Format order headers

--- app/components/Orders.jsx
+++ app/components/Orders.jsx
@@ -1,20 +1,45 @@
 import React, {Component} from 'react';
+import moment from 'moment';
 
 export class Orders extends Component {
   componentDidMount () {
     this.props.getOrderHistory();
   }
+  formatDate (date) {
+    return moment(date).format('MMMM D, YYYY').toString();
+  }
+  capitalizeOrderStatus (status) {
+    return status.charAt(0).toUpperCase() + status.slice(1);
+  }
   render () {
     return (
-      <div>
+      <div className="container">
         <h1>Your Order History</h1>
-        <p>Click on any order for details.</p>
         {this.props.orderHistory && this.props.orderHistory.map(order => (
           <div key={order.orderID}>
-            <h2>Order ID: {order.orderID}</h2>
-            <p>Ordered On: {order.orderDate}</p>
-            <p>Status: {order.status}</p>
-            <p>Shipping Address: {order.shippingAddress}</p>
+            <div className="order-header">
+              <div className="row">
+                <div className="col-xs-8">
+                  <h4>Order ID: {order.orderID}</h4>
+                </div>
+                <div className="col-xs-4 align-right">
+                  <h4>Placed: {this.formatDate(order.orderDate)}</h4>
+                </div>
+              </div>
+            </div>
+            <div className="order-subheader">
+              <div className="row">
+                <div className="col-xs-3">
+                  <p><strong>Total:</strong> ${order.totalPrice}</p>
+                </div>
+                <div className="col-xs-3">
+                  <p><strong>Status:</strong> {this.capitalizeOrderStatus(order.status)}</p>
+                </div>
+                <div className="col-xs-6 align-right">
+                  <p><strong>Ship To:</strong> {order.shippingAddress}</p>
+                </div>
+              </div>
+            </div>
             <h3>Details</h3>
             {order.products.map(product => (
               <div key={product.id}>
