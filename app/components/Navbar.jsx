3c7deb87b156dfb2239424dba624eda3d4009926
Joey Darbyshire <Josephmdarbyshire@gmail.com>
2017-01-13 23:34:03 -0500

    sass setup

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -8,6 +8,8 @@ export default function (props) {
   return (
     <div>
       <nav>
+        <div className="col-xs-8">Test</div>
+        <div className="col-xs-4">Test</div>
         {props.user ? null : <Signup/>}
         {props.user ? <WhoAmI/> : <Login/>}
       </nav>
