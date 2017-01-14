6d0460427c6239dcd7786ab5376328500d362722
Beth Qiang <bethany.qiang@gmail.com>
2017-01-14 01:04:32 -0600

    style: create order history layout
    - Change name of style.scss to main.
    - Change build scripts in package.json so they find all stylesheets, not just the primary one.
    - Add orders.scss.
    - Functionality and futher styling still needed.

--- app/components/Orders.jsx
+++ app/components/Orders.jsx
@@ -13,24 +13,27 @@ export class Orders extends Component {
   }
   render () {
     return (
-      <div className="container">
+      <div className="order-container">
         <h1>Your Order History</h1>
         {this.props.orderHistory && this.props.orderHistory.map(order => (
-          <div key={order.orderID}>
+          <div key={order.orderID} className="single-order">
             <div className="order-header">
               <div className="row">
                 <div className="col-xs-8">
-                  <h4>Order ID: {order.orderID}</h4>
+                  <h5>
+                    <span className="glyphicon glyphicon glyphicon-list-alt"></span>
+                    Order ID: {order.orderID}
+                  </h5>
                 </div>
                 <div className="col-xs-4 align-right">
-                  <h4>Placed: {this.formatDate(order.orderDate)}</h4>
+                  <h5>Placed: {this.formatDate(order.orderDate)}</h5>
                 </div>
               </div>
             </div>
             <div className="order-subheader">
               <div className="row">
                 <div className="col-xs-3">
-                  <p><strong>Total:</strong> ${order.totalPrice}</p>
+                  <p><strong>Total:</strong> ${order.totalPrice / 100}</p>
                 </div>
                 <div className="col-xs-3">
                   <p><strong>Status:</strong> {this.capitalizeOrderStatus(order.status)}</p>
@@ -40,16 +43,26 @@ export class Orders extends Component {
                 </div>
               </div>
             </div>
-            <h3>Details</h3>
-            {order.products.map(product => (
-              <div key={product.id}>
-                <img src={`${product.images[0]}`} />
-                <p>{product.name}</p>
-                <p>Quantity: {product.orderproducts.quantity}</p>
-                <p>Price: {product.price}</p>
-              </div>
-            ))}
-            <hr />
+            <div className="product-details">
+              <h3>Details</h3>
+              {order.products.map(product => (
+                <div key={product.id} className="row single-product">
+                  <div className="col-xs-3">
+                    <img src={`${product.images[0]}`} />
+                  </div>
+                  <div className="col-xs-4">
+                    { /* name should link to product when FE product route is completed */ }
+                    <p>{product.name}</p>
+                    { /* this button is currently not functional */ }
+                    <button>Buy It Again</button>
+                  </div>
+                  <div className="col-xs-5 align-right">
+                    <p>Quantity: {product.orderproducts.quantity}</p>
+                    <p>Price: ${product.price / 100}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         ))}
       </div>
