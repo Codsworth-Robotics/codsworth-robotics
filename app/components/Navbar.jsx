e4d5c71a9ae6422b866ece3d7a6faa014624bf77
Joey Darbyshire <Josephmdarbyshire@gmail.com>
2017-01-14 17:06:24 -0500

    Navbar basis created.  Jquery added.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,13 +6,59 @@ import Login from './Login';
 
 export default function (props) {
   return (
-    <div>
-      <nav>
-        <div className="col-xs-8">Test</div>
-        <div className="col-xs-4">Test</div>
-        {props.user ? null : <Signup/>}
-        {props.user ? <WhoAmI/> : <Login/>}
-      </nav>
-    </div>
+    <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        { /* Brand and toggle get grouped for better mobile display */ }
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="#">Brand</a>
+        </div>
+        { /* Collect the nav links, forms, and other content for toggling */ }
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul className="nav navbar-nav">
+            <li className="dropdown">
+              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Shop <span className="caret"></span></a>
+              <ul className="dropdown-menu">
+                <li><b>Categories</b></li>
+                <li><a href="#">All Products</a></li>
+                <li><a href="#">Butlers</a></li>
+                <li><a href="#">Chefs</a></li>
+                <li><a href="#">Gardeners</a></li>
+                <li role="separator" className="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form className="navbar-form navbar-left">
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="Search"/>
+            </div>
+            <button type="submit" className="btn btn-default">Submit</button>
+          </form>
+          <ul className="nav navbar-nav navbar-right">
+            { /* Login and Logout are here temporarily, they will be moved to the Account dropdown menu eventually */}
+            <li className="nav-user">{props.user ? null : <Signup/>}{props.user ? <WhoAmI/> : <Login/>}</li>
+            <li className="dropdown">
+              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account <span className="caret"></span></a>
+              <ul className="dropdown-menu">
+                <li><a href="#">Your Account</a></li>
+                <li><a href="#">Your Orders</a></li>
+                <li><a href="#">Logout</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Cart</a></li>
+          </ul>
+        </div>{ /* /.navbar-collapse */ }
+      </div>{ /* /.container-fluid */ }
+    </nav>
   );
 }
+// <div className="col-xs-8">Test</div>
+//         <div className="col-xs-4">Test</div>
+//         {props.user ? null : <Signup/>}
+//         {props.user ? <WhoAmI/> : <Login/>}
--- app/main.jsx
+++ app/main.jsx
@@ -2,30 +2,14 @@
 import React from 'react';
 import {Router, Route, IndexRedirect, browserHistory} from 'react-router';
 import {render} from 'react-dom';
-import {connect, Provider} from 'react-redux';
+import {Provider} from 'react-redux';
 
 import store from './store';
 import App from './containers/App';
 import Jokes from './components/Jokes';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import WhoAmI from './components/WhoAmI';
 import Checkout from './components/Checkout';
 import AddProduct from './components/AddProduct';
 import Orders from './components/Orders';
-
-const ExampleApp = connect(
-  ({ auth }) => ({ user: auth })
-)(
-  ({ user, children }) =>
-    <div>
-      <nav>
-        {user ? null : <Signup/>}
-        {user ? <WhoAmI/> : <Login/>}
-      </nav>
-      {children}
-    </div>
-);
 
 render(
   <Provider store={store}>
