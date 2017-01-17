5c4fd5c80f0eb5aaea7796f209d72015fadd683a
Beth Qiang <bethany.qiang@gmail.com>
2017-01-16 22:03:42 -0600

    Alfred Pennyworth sends you a confirmation email

--- app/components/SuccessfulOrder.jsx
+++ app/components/SuccessfulOrder.jsx
@@ -5,7 +5,7 @@ const SuccessfulOrder = () => {
     <div>
       <h2>Thank you for your order!</h2>
       <p>Your order # is 23904892304820948. (But that's not actually your order number.)</p>
-      <p>A confirmation email has been sent to the email you provided. (But that email hasn't actually been sent.)</p>
+      <p>A confirmation email has been sent to the email you provided.</p>
     </div>
   );
 };
--- server/api/orders.js
+++ server/api/orders.js
@@ -1,14 +1,19 @@
 const router = require('express')();
 const Order = require('APP/db/models/order');
 const Product = require('APP/db/models/product');
+const sendEmail = require('APP/server/send-email');
 
 router.post('/', (req, res, next) => {
+  const email = req.body.email;
+  let orderNum;
+
   Order.create({
     email: req.body.email,
     shippingAddress: req.body.shippingAddress,
     user_id: req.user && req.user.id
   })
   .then(order => {
+    orderNum = order.orderID;
     return req.session.cart.products.map(product => {
       return Product.findById(product.id)
       .then(foundProduct =>
@@ -17,9 +22,16 @@ router.post('/', (req, res, next) => {
     });
   })
   .then(order => {
+    sendEmail(
+      email,
+      'Your Recent Codsworth Robotics Order',
+      `"Drive sports cars, date movie stars, buy things that are not for sale... who knows? You start pretending to have fun, you might even have a little by accident."
+      Thank you for your recent Codsworth Robotics order.
+      Your order number is ${orderNum}. Please keep this for your records.`
+      );
     res.json(order);
   })
-  .catch(error => console.error(error));
+  .catch(next);
 });
 
 module.exports = router;
