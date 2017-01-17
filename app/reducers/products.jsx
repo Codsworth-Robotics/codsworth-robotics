eedc57e11518d3ee1664853c0e90ab71ab77debd
Mark Hario <hario.mark@gmail.com>
2017-01-17 16:32:41 -0500

    added reviews box

--- app/reducers/products.jsx
+++ app/reducers/products.jsx
@@ -18,10 +18,19 @@ export const selectProductReducer = (state = {}, action) => {
   }
 };
 
+export const reviewsReducer = (state = [], action) => {
+  switch (action.type) {
+    case RECEIVE_REVIEWS:
+      return action.reviews;
+    default: return state;
+  }
+};
+
 /* ------------- ACTION TYPES ------------------- */
 
 const RECEIVE_ITEMS = 'RECEIVE_ITEMS';
 const SELECT_PRODUCT = 'SELECT_PRODUCT';
+const RECEIVE_REVIEWS = 'RECEIVE_REVIEWS';
 
 /* ------------- ACTION CREATORS ---------------- */
 
@@ -31,6 +40,10 @@ export const getProducts = (products) => {
 
 export const setSelectedProduct = (product) => {
   return { type: SELECT_PRODUCT, product: product };
+};
+
+export const getReviews = (reviews) => {
+  return { type: RECEIVE_REVIEWS, reviews: reviews };
 };
 
 // request all products
@@ -51,3 +64,14 @@ export const loadProducts = (id = null) => {
     })
     .catch(err => console.error(err));
 };
+
+export const loadReviews = (productId) => {
+  return dispatch =>
+  axios.get(`/api/products/reviews/${productId}`)
+  .then(res => res.data)
+  .then(reviews => {
+    dispatch(getReviews(reviews));
+  })
+  .catch(err => console.error(err));
+};
+
