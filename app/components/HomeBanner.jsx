3e6692e0ae6211aabb7415b596ec293612e306b2
Joey Darbyshire <Josephmdarbyshire@gmail.com>
2017-01-18 09:45:06 -0500

    carousel updated

--- app/components/HomeBanner.jsx
+++ app/components/HomeBanner.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router';
+
 export default function (props) {
   return (
     <div id="home-banner" className="row">
@@ -12,19 +14,19 @@ export default function (props) {
         { /* Wrapper for slides */ }
         <div className="carousel-inner">
           <div className="item active">
-            <img src="images/boxie-slider-headphones.jpg" alt="demo_slide_2" />
+            <img src="images/starwars.png" alt="demo_slide_2" />
             <div className="jumbotron carousel-caption">
-              <h1><i className="fa fa-cube"></i> Boxie!</h1>
-              <p>A Responsive LemonStand Theme.</p>
-              <p><a href="#" className="btn btn-primary btn-lg" role="button">Learn More &raquo;</a></p>
+              <h1><i className="fa fa-cube"></i> Star Wars!</h1>
+            <p>New R2D2 Mechanic coming soon!</p>
+              <p><Link to="/products/2" className="btn btn-primary btn-lg" role="button">Buy C3P0 Now &raquo;</Link></p>
             </div>
           </div>
           <div className="item">
-            <img src="images/boxie-slider-speakers.jpg" alt="demo_slide_1" />
+            <img src="images/butlers.png" alt="demo_slide_1" />
             <div className="jumbotron carousel-caption left">
-              <h1>Speakers</h1>
-              <p>Cube speakers. Awesome.</p>
-              <p><a href="#" className="btn btn-primary btn-lg" role="button">Shop Now &raquo;</a></p>
+              <h1>Butlers!</h1>
+              <p>Browse our awesome selection of Butlers!</p>
+              <p><Link to="/products?category=butler" className="btn btn-primary btn-lg" role="button">Shop Now &raquo;</Link></p>
             </div>
           </div>
         </div>
